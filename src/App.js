eb8f55972efdc3115dd42ad3f4c80b82c31813da
VardhanYerubandi <yerubandivardhan@gmail.com>
2025-08-05 16:31:44 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1> Welcome to CICD class</h1>
+      <h1 className="typewriter">Welcome to CICD class</h1>
+      <h5 className="typewriter delay">by VardhanYerubandi</h5>
     </div>
   );
 }
